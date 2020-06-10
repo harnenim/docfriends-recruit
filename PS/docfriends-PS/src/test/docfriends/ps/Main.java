6ec547b4dc42dde507ae94623245b006298c77dd
Harne Nim <harne_@naver.com>
2020-06-10 18:59:50 +0900

    최초 커밋

---
+++ PS/docfriends-PS/src/test/docfriends/ps/Main.java
@@ -0,0 +1,21 @@
+package test.docfriends.ps;
+
+public class Main {
+	
+	/*
+	 * 숫자와 문자가 섞인 q라는 문자열이 있는데
+	 * 해당 문자열에서 숫자의 배열을 numbers라고 할때
+	 * 가장 작은 숫자가 numbers의 a번째, 가장 큰 숫자가 numbers의 b번째에 있다고 한다.
+	 * a + b의 합을 구하여라.
+	 */
+	public static int solve(String q) {
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		String q = "ab23c4d56e78f9g12h34i5j12k45l67n89m99o1k123p456q567r768s890t67u456v345w234x23y239z";
+		int output = solve(q);
+		System.out.println(output);
+	}
+
+}
