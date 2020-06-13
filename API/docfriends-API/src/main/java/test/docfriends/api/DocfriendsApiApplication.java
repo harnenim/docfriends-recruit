d1af4f3b8432549919f5571c93850debe5ba913f
Harne Nim <harne_@naver.com>
2020-06-13 15:56:44 +0900

    spring boot 기본

---
+++ API/docfriends-API/src/main/java/test/docfriends/api/DocfriendsApiApplication.java
@@ -0,0 +1,13 @@
+package test.docfriends.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocfriendsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocfriendsApiApplication.class, args);
+	}
+
+}
---
+++ API/docfriends-API/src/test/java/test/docfriends/api/DocfriendsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package test.docfriends.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocfriendsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
